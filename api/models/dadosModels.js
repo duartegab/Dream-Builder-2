9ed1d8b3d89f22cd52f9c073209175eb8fd12bc6
Gabriel Duarte <gabrielgoncalvesduarte5@gmail.com>
2024-11-14 11:29:32 -0300

    criação das rotas de consulta - edições e cursos

--- api/models/dadosModels.js
+++ api/models/dadosModels.js
@@ -5,6 +5,11 @@ console.log("Acessando Models das Edições...")
 
 module.exports = {
     listarEdicoes,
+    listarAds,
+    listarDsm,
+    listarGe,
+    listarGpi,
+    listarGrh
 }
 
 
@@ -12,6 +17,36 @@ module.exports = {
 
 function listarEdicoes(callback) {
     m_sql = 'select * from edicao';
+
+    conexao.query(m_sql, callback)
+}
+
+function listarAds(callback) {
+    m_sql = 'select * from cursos Where nome_curso = "ADS"';
+
+    conexao.query(m_sql, callback)
+}
+
+function listarDsm(callback) {
+    m_sql = 'select * from cursos Where nome_curso = "DSM"';
+
+    conexao.query(m_sql, callback)
+}
+
+function listarGe(callback) {
+    m_sql = 'select * from cursos Where nome_curso = "GE"';
+
+    conexao.query(m_sql, callback)
+}
+
+function listarGpi(callback) {
+    m_sql = 'select * from cursos Where nome_curso = "GPI"';
+
+    conexao.query(m_sql, callback)
+}
+
+function listarGrh(callback) {
+    m_sql = 'select * from cursos Where nome_curso = "GRH"';
 
     conexao.query(m_sql, callback)
 }
