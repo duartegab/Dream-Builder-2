b8f7662e704c7fc6538a87e204f9b7efb2cc1ed0
Gabriel Duarte <gabrielgoncalvesduarte5@gmail.com>
2024-11-18 23:26:44 -0300

    atualizando configuração de conexão original

--- api/config/conexao.js
+++ api/config/conexao.js
@@ -3,7 +3,7 @@ var database = "dreambuilder";
 
 var conexao = mysql.createConnection({
     user: 'root',
-    password: 'fatec123*',
+    password: '13579',
     host: 'localhost',
     port: 3306
 });
